2fca21b4494b44c3c8f2356161e0f823621cf24e
Christophe Lincoln <pankso@slitaz.org>
2012-05-20 01:40:43 +0200

    move internal vars to libcook.sh and broken function

--- lib/libcook.sh
+++ lib/libcook.sh
@@ -13,3 +13,35 @@
 # System wide config can be overwriten by a cook.conf in current path.
 [ -f "/etc/slitaz/cook.conf" ] && . /etc/slitaz/cook.conf
 [ -f "cook.conf" ] && . ./cook.conf
+
+# Shared DB between Cook, the Cooker and Cookiso.
+# In cookiso: repo= --> flavors
+flavors="$SLITAZ/flavors"
+activity="$CACHE/activity"
+commits="$CACHE/commits"
+cooklist="$CACHE/cooklist"
+cookorder="$CACHE/cookorder"
+command="$CACHE/command"
+blocked="$CACHE/blocked"
+broken="$CACHE/broken"
+cooknotes="$CACHE/cooknotes"
+crontabs="/var/spool/cron/crontabs/root"
+
+# Lograte activity.
+[ -s "$activity" ] && tail -n 60 $activity > /tmp/tail-$$ && \
+	mv -f /tmp/tail-$$ $activity
+
+# Log activities, we want first letter capitalized.
+# TODO: use /lib/libtaz.sh log() but need to change all:
+# echo "Message" | log --> log "Message"
+log() {
+	grep ^[A-Z] | \
+		sed s"#^[A-Z]\([^']*\)#$(date '+%Y-%m-%d %H:%M') : \0#" >> $activity
+}
+
+# Log broken packages.
+broken() {
+	if ! grep -q "^$pkg$" $broken; then
+		echo "$pkg" >> $broken
+	fi
+}
