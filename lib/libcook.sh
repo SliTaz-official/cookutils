62a4c0cec186f05654f78218ef44724ae8be6657
Christopher Rogers <slaxemulator@gmail.com>
2015-04-14 04:34:10 +0000

    lib/libcook.sh: Add typo fixes from default branch.

--- lib/libcook.sh
+++ lib/libcook.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # Cook library - Shared configs and functions between cook, the cooker and
-# cookiso. Read the README before adding or modifing any code in libcook.sh!
+# cookiso. Read the README before adding or modifying any code in libcook.sh!
 #
 # Copyright (C) SliTaz GNU/Linux - GNU gpl v3
 # Author: Christophe Lincoln <pankso@slitaz.org>
@@ -10,7 +10,7 @@
 . /usr/lib/slitaz/libpkg.sh
 . /etc/slitaz/slitaz.conf
 
-# System wide config can be overwriten by a cook.conf in current path.
+# System wide config can be overwritten by a cook.conf in current path.
 [ -f "/etc/slitaz/cook.conf" ] && . /etc/slitaz/cook.conf
 [ -f "cook.conf" ] && . ./cook.conf
 
